036de707062e7ccf856c61bf28688c93a837966d
turtletoastdude <thallesdesouza24@gmail.com>
2025-08-07 00:13:12 -0400

    Update Download_XSpaces_to_MP3.bat
    Added progress bar

--- Download_XSpaces_to_MP3.bat
+++ Download_XSpaces_to_MP3.bat
@@ -1,13 +1,13 @@
 
 @echo off
-title X Spaces to MP3 Downloader
+title X Spaces to MP3 Downloader (Standard - With Progress)
 echo -----------------------------------------
 echo Paste your Twitter/X Space link below:
 set /p link=
 
 echo.
-echo Downloading and converting to MP3...
-yt-dlp.exe -x --audio-format mp3 --ffmpeg-location . %link%
+echo Downloading and converting to original quality MP3...
+yt-dlp.exe --progress -x --audio-format mp3 --ffmpeg-location . %link%
 
 echo.
 echo Done! Your MP3 file is saved in this folder.
